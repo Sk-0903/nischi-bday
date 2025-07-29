a8550c6defd2d844b51464e93f98c69efa73feab
Sk-0903 <keshavsavanth302@gmail.com>
2025-07-29 22:16:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function goToSurprise() {
   window.location.href = "enter-name.html";
 }
+function startMusicAndGo() {
+  const audio = new Audio("bg_music.mp3");
+  audio.loop = true;
+  audio.volume = 0.5;
+  audio.play();
+
+  localStorage.setItem("musicPlaying", "true");
+
+  setTimeout(() => {
+    window.location.href = "enter-name.html";
+  }, 500); // short delay to allow audio to begin
+}
 
