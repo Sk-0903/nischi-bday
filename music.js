7ff3f80a3db5be5fc0247b563b18f644a7809398
Sk-0903 <keshavsavanth302@gmail.com>
2025-07-29 22:10:40 +0530

    Update music.js

--- music.js
+++ music.js
@@ -2,7 +2,7 @@
 if (!localStorage.getItem("musicPlaying")) {
   const audio = new Audio("bg_music.mp3");
   audio.loop = true;
-  audio.volume = 2.0;
+  audio.volume = 100;
   audio.play();
 
   // Store reference in window so it doesn’t restart
